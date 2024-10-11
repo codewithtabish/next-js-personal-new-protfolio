93c27d019fcb004c13919f7dce6b3ca41a2efa6d
codewithzain099 <zainiikhan099@gmail.com>
2024-10-10 20:02:12 -0700

    hero info section basic design has been done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,15 @@
 /** @format */
 
+import HeroInfoSection from '@/components/custom/home/HeroInfoSection';
 import { outfit, lusitana } from '@/ui/fonts';
 import Image from 'next/image';
 import React from 'react';
 const HomePage = () => {
   return (
-    <main className='flex flex-col min-h-[100vh] space-y-16'>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio dolore
-      reprehenderit architecto, consequatur odio dolorum similique tempora
-      commodi amet, modi, id consequuntur velit. Repellendus modi corrupti iure
-      veniam fugiat voluptatum? Lorem ipsum dolor sit amet consectetur
-      adipisicing elit. Distinctio dolore reprehenderit architecto, consequatur
-      odio dolorum similique tempora commodi amet, modi, id consequuntur velit.
-      Repellendus modi corrupti iure veniam fugiat voluptatum? Lorem ipsum dolor
-      sit amet consectetur adipisicing elit. Distinctio dolore reprehenderit
-      architecto, consequatur odio dolorum similique tempora commodi amet, modi,
-      id consequuntur velit. Repellendus modi corrupti iure veniam fugiat
-      voluptatum?
-      <hr />
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora est
-        vero exercitationem inventore, commodi nulla ullam eveniet mollitia id
-        distinctio dolores porro quisquam numquam sequi similique magni
-        voluptatibus asperiores explicabo Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Laborum, facilis eveniet. Eaque ipsa delectus, sint
-        animi laudantium aliquam odio, distinctio corporis a quam deserunt
-        asperiores voluptatibus in nihil architecto minus.
-      </p>
+    <main className='flex flex-col min-h-[100vh] py-16 max-w-4xl mx-auto'>
+      <section>
+        <HeroInfoSection />
+      </section>
     </main>
   );
 };
--- src/components/custom/home/HeroInfoSection.tsx
+++ src/components/custom/home/HeroInfoSection.tsx
@@ -1,49 +1,50 @@
 /** @format */
+'use client';
+import React from 'react';
+import BlurFade from '@/components/ui/blur-fade'; // default export
+import BlurFadeText from './blur-fade-text'; // default export
+import { Avatar, AvatarImage } from './avatar'; // named exports
 
-// /** @format */
+import { DATA } from '@/data/resume';
 
-// import React from 'react';
-// import BlurFadeText from '@/components/magicui/blur-fade-text';
-// import BlurFade from '@/components/magicui/blur-fade';
-// import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+const HeroInfoSection = () => {
+  // const response = await fetch('http://localhost:3000/api/peronalinfo', {
+  //   cache: 'force-cache',
+  // });
+  // const data = await response.json();
+  const BLUR_FADE_DELAY = 0.04;
 
-// const HeroInfoSection = async () => {
-//   const response = await fetch('http://localhost:3000/api/peronalinfo', {
-//     cache: 'force-cache',
-//   });
-//   const data = await response.json();
-//   const BLUR_FADE_DELAY = 0.04;
+  return (
+    <div className='mx-auto w-full max-w-2xl space-y-8'>
+      <div className='gap-2 flex justify-between'>
+        <div className='flex-col flex flex-1 space-y-1.5'>
+          <BlurFadeText
+            delay={BLUR_FADE_DELAY}
+            className='text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none'
+            yOffset={8}
+            text={`Hi, I'm ${DATA?.name?.split(' ')[0]} 👋`}
+          />
+          <BlurFadeText
+            // animateByCharacter={true}
+            // characterDelay={0.05}
+            className='max-w-[600px] overflow-x-hidden md:text-xl'
+            delay={BLUR_FADE_DELAY}
+            text={DATA.description}
+          />
+        </div>
+        <BlurFade delay={BLUR_FADE_DELAY}>
+          <Avatar className='size-28 border'>
+            <AvatarImage
+              className='object-cover'
+              alt={DATA.name}
+              src={DATA.avatarUrl}
+            />
+            {/* <AvatarFallback>{DATA.initials}</AvatarFallback> */}
+          </Avatar>
+        </BlurFade>
+      </div>
+    </div>
+  );
+};
 
-//   return (
-//     <div className='mx-auto w-full max-w-2xl space-y-8'>
-//       <div className='gap-2 flex justify-between'>
-//         <div className='flex-col flex flex-1 space-y-1.5'>
-//           <BlurFadeText
-//             delay={BLUR_FADE_DELAY}
-//             className='text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none'
-//             yOffset={8}
-//             text={`Hi, I'm ${data?.info?.name?.split(' ')[0]} 👋`}
-//           />
-//           <BlurFadeText
-//             // animateByCharacter={true}
-//             // characterDelay={0.05}
-//             className='max-w-[600px] overflow-x-hidden md:text-xl'
-//             delay={BLUR_FADE_DELAY}
-//             text={data?.info?.subtitle}
-//           />
-//         </div>
-//         <BlurFade delay={BLUR_FADE_DELAY}>
-//           <Avatar className='size-28 border'>
-//             <AvatarImage
-//               alt={data?.info?.name}
-//               src={data?.info?.profile_image}
-//             />
-//             {/* <AvatarFallback>{DATA.initials}</AvatarFallback> */}
-//           </Avatar>
-//         </BlurFade>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default HeroInfoSection;
+export default HeroInfoSection;
