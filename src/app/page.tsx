bb6f66ae2e323bd477dc5c93dada20b053521236
codewithzain099 <zainiikhan099@gmail.com>
2024-10-10 19:06:47 -0700

    wrote some other text in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 import React from 'react';
 const HomePage = () => {
   return (
-    <main className='flex flex-col min-h-[100dvh] space-y-16'>
+    <main className='flex flex-col min-h-[100vh] space-y-16'>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio dolore
       reprehenderit architecto, consequatur odio dolorum similique tempora
       commodi amet, modi, id consequuntur velit. Repellendus modi corrupti iure
@@ -17,6 +17,16 @@ const HomePage = () => {
       architecto, consequatur odio dolorum similique tempora commodi amet, modi,
       id consequuntur velit. Repellendus modi corrupti iure veniam fugiat
       voluptatum?
+      <hr />
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora est
+        vero exercitationem inventore, commodi nulla ullam eveniet mollitia id
+        distinctio dolores porro quisquam numquam sequi similique magni
+        voluptatibus asperiores explicabo Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Laborum, facilis eveniet. Eaque ipsa delectus, sint
+        animi laudantium aliquam odio, distinctio corporis a quam deserunt
+        asperiores voluptatibus in nihil architecto minus.
+      </p>
     </main>
   );
 };
